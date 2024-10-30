307d77048f3555d4ef14f01ac61df461a79a0984
Jose <javierserrano092@gmail.com>
2024-10-29 23:29:28 -0500

    Boléanos

--- Scrip.js
+++ Scrip.js
@@ -1,5 +1,18 @@
 
 // Variables
-var nombre = "Jose Javier";
-var apellidos = "Serrano Vasquez"
-console.log("Nombres :" + nombre + "Apellidos :" + apellidos)
+let nombre = "Jose Javier";
+let apellidos = "Serrano Vasquez"
+console.log("Nombres :" + nombre)
+console.log("Apellidos :" + apellidos)
+
+// Boléanos
+
+let esDia = true;
+
+if (esDia) {
+    console.log("Es de día.");
+} 
+else {
+    console.log("Es de noche.");
+}
+ 
