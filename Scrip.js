dbd3a0b8ff228942aee77b36311358c0aeec9745
Jose <javierserrano092@gmail.com>
2024-10-29 23:40:11 -0500

    Decimales

--- Scrip.js
+++ Scrip.js
@@ -19,7 +19,7 @@ else {
 // Numeros enteros
 
 let numero1 = 50;
-let numero2 =60;
+let numero2 = 60;
 
 
 let suma = numero1 + numero2;
@@ -28,4 +28,15 @@ console.log("La suma es:", suma);
 
 let resta = numero1 - numero2;
 console.log("La resta es:", resta); 
+
+// Decimales
+
+let numero3 = 10.4;
+let numero4 = 6.6;
+
+let multiplicacion = numero3 * numero4;
+console.log("La multiplicación es:", multiplicacion); 
+
+let division = numero3 / numero4;
+console.log("La división es:", division); 
 
