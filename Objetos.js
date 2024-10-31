e54b399b4e8aadc436a32c8d9b3c853203a7d875
iJavierSerrano <142032121+iJavierSerrano@users.noreply.github.com>
2024-10-30 19:12:52 -0500

    Primer commit

---
+++ Objetos.js
@@ -0,0 +1,17 @@
+ 
+ 
+ const usuario = {
+
+    nombre: "Javier",
+    edad: 25,
+    trabaja: true, 
+ }
+    // Imprimir las propiedades de un objeto:
+    console.log(usuario);
+
+    // Imprimir propiedades por separado:
+    console.log(usuario.nombre);
+    console.log(usuario.edad);
+    console.log(usuario.trabaja);
+
+    
