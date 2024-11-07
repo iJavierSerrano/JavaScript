b391b9c7c9b8547be0eb5b6550f7de5cca6d52b5
iJavierSerrano <142032121+iJavierSerrano@users.noreply.github.com>
2024-11-06 21:33:29 -0500

    cambio codigo

--- Empleado.js
+++ Empleado.js
@@ -5,17 +5,26 @@ class Empleado {
         this.puesto = puesto;
     }
 
-    incrementarSalario() {
-        this.salario += 100000;
-        return `El salario de ${this.nombre} ha sido incrementado en 100000.`;
+    // Método para incrementar el salario en porcentaje
+    incrementarSalario(percentage) {
+        const incremento = (this.salario * percentage) / 100;
+        this.salario += incremento;
+        return `El salario de ${this.nombre} ha sido incrementado en ${percentage}% (valor: ${incremento}). salario actual ${this.salario}`;
     }
 
-    datosEmpleados() {
+    // Método para mostrar los datos del empleado
+    mostrarDatos() {
         return `El empleado ${this.nombre} tiene un salario de ${this.salario} y ocupa un puesto como ${this.puesto}.`;
     }
 }
 
+// Crear un objeto Empleado
 let empl = new Empleado("Javier", 500000, "Despachos");
-console.log(empl.incrementarSalario());
-console.log(empl.datosEmpleados());
+console.log(empl.incrementarSalario(10)); // Incremento del 10%
+console.log(empl.mostrarDatos());
+
+// Ejemplo con un incremento personalizado
+console.log(empl.incrementarSalario(5)); // Incremento del 5%
+console.log(empl.mostrarDatos());
+
 
